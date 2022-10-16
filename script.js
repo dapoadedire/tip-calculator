d7e2eb2a2de2d8217af55741702fc83ddf47d8aa
dapoadedire <adedireadedapo19@gmail.com>
2022-10-16 20:56:34 +0100

    feat: done

--- script.js
+++ script.js
@@ -3,16 +3,21 @@ const tipPercentElement = document.querySelector("#tip-percent");
 const submitButtonElement = document.querySelector("#submit-button");
 const remainingMoneyElement = document.querySelector("#remaining-money");
 const tipAmountElement = document.querySelector("#tip-amount");
+const errorMessageElement = document.querySelector("#error-message");
 
 const tipCalculator = (totalMoney, tipPercent) => {
     if (totalMoney < 0 || tipPercent < 0) {
-        alert("Please enter a positive number");
+        // alert("Please enter a positive number");
+        errorMessageElement.textContent = "Please enter a positive number";
     } 
     else if (totalMoney == 0 && tipPercent == 0) {
-        alert("Please enter a number greater than 0");
+        // alert("Please enter a number greater than 0");
+        errorMessageElement.textContent = "Please enter a number greater than 0";
+    
     }
     else if (tipPercent > 100) {
-        alert("Please enter a tip percentage less than 100 or equal to 100");
+        // alert("Please enter a tip percentage less than 100 or equal to 100");
+        errorMessageElement.textContent = "Please enter a tip percentage less than 100 or equal to 100";
     }
     
     else {
@@ -20,6 +25,7 @@ const tipCalculator = (totalMoney, tipPercent) => {
         const remainingMoney = parseFloat(totalMoney) + parseFloat(tipAmount);
         tipAmountElement.textContent = tipAmount.toFixed(2) ;
         remainingMoneyElement.textContent = remainingMoney.toFixed(2) ;
+        errorMessageElement.textContent = "";
     }
 
 }
